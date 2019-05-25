25e1d063d58138c3cc9bf6d89cfc4e9290f6a27d
Evaldas <evaldasmontvydas@gmail.com>
2019-05-25 21:20:50 +0300

    nd 12

---
+++ js_test/js/scripts.js
@@ -0,0 +1,9 @@
+function changeNameToSurName() {
+    // document.getElementById('name').innerHTML = 5+5;
+    var myName = 'Evaldas';
+    var mySurName = 'Montvydas';
+    var age = 50;
+    var ageAfterNYrs = age + 5;
+    console.log(myName + " " + mySurName);
+    console.log(ageAfterNYrs);
+}
